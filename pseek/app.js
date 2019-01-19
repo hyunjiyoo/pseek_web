85356d045757c36b55b907d873c21ea8c60e3094
hyunjiyoo <solteee23@gmail.com>
2019-01-19 19:10:36 +0900

    .

--- pseek/app.js
+++ pseek/app.js
@@ -2,16 +2,6 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-// app.use(express.static(__dirname + '/public'));
-// app.use('/', (req, res, next) => {
-//     fs.readFile('header-bg.jpg', (err, data) => {
-//         res.writeHead(200, {'Content-Type' : 'image/jpg'});
-//         console.log(data);
-//         res.send(data);
-//     });
-//     next();
-// });
-
 app.get('/', (req, res) => {
     fs.readFile('index.html', 'utf8', (err, data) => {
         res.writeHead(200, {'Content-Type' : 'text/html'});
