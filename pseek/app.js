f972bcbff7e7e9abd34bdb6253b983eac102454f
byungki676 <byungki676@gmail.com>
2019-01-21 11:41:10 +0900

    ejs.render가져옴

--- pseek/app.js
+++ pseek/app.js
@@ -2,21 +2,23 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 
-// app.use(express.static(__dirname + '/public'));
-// app.use('/', (req, res, next) => {
-//     fs.readFile('header-bg.jpg', (err, data) => {
-//         res.writeHead(200, {'Content-Type' : 'image/jpg'});
-//         console.log(data);
-//         res.send(data);
-//     });
-//     next();
-// });
+/*
+* ejs view 사용
+* */
+app.set("view engine", "ejs");
+
+/*
+* default 경로 . 선언
+* */
+app.use(express.static(__dirname + "/."));
+
 
 app.get('/', (req, res) => {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.end(data);
-    });
+    // fs.readFile('index.html', 'utf8', (err, data) => {
+    //     res.writeHead(200, {'Content-Type' : 'text/html'});
+    //     res.end(data);
+    // });
+    res.render("index");
 });
 
 app.listen(30000, () => {
