047ab5a17042dfbe40216979b5eb59257d976a2a
hyunjiyoo <solteee23@gmail.com>
2019-01-21 11:16:06 +0900

    ejs.render 해결

--- pseek/app.js
+++ pseek/app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var fs = require('fs');
+var ejs = require('ejs');
 var app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
 app.get('/', (req, res) => {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-        res.writeHead(200, {'Content-Type' : 'text/html'});
-        res.end(data);
-    });
+    res.render("index.ejs",{});
+    // fs.readFile('index.ejs', 'utf8', (err, data) => {
+    //     //res.writeHead(200, {'Content-Type' : 'text/html'});
+    //     //res.end(ejs.render(data));
+    //
+    // });
+});
+app.get('/myPage', (req, res) => {
+    res.render("myPage.ejs", {});
 });
 
 app.listen(30000, () => {
