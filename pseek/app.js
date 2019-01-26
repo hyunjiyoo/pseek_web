a0ee9a53c1b2f7cfb799325567e242873bbc59e2
hyunjiyoo <solteee23@gmail.com>
2019-01-27 05:12:43 +0900

    login, logout 구현

--- pseek/app.js
+++ pseek/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
-// var fs = require('fs');
-// var ejs = require('ejs');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var app = express();
 var db = require('./lib/db.js');
 
@@ -12,8 +12,29 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
 
+// 세션 미들웨어 저장
+app.use(session({
+    // 사용자가 js 입력못하게 보안
+    HttpOnly: true,
+    // https 로 접속
+    secure: true,
+    // 필수 옵션으로 secret 키 지정
+    secret: 'asdfafsa!!dfgasg@#$fdg',
+    // 세션데이터값 바뀌기 전까지는 세션저장소값에 저장암함.
+    resave: false,
+    // 세션필요하기전까지 세션구동 안하게 설정
+    saveUninitialized: true,
+    // 파일에 세션정보 저장
+    store: new FileStore()
+}));
+
+app.use(function(req, res, next) {
+    res.locals.user = req.session.userId;
+    res.locals.isLogined = req.session.is_Logined;
+    next();
+});
 app.use('/', require("./routes/home"));
 
-app.listen(30010, () => {
+app.listen(30000, () => {
     console.log('server running');
 });
