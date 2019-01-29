2449cda2bc9116060acb6417af675d8d33662b7e
byungki676 <byungki676@gmail.com>
2019-01-29 10:14:01 +0900

    reload

--- pseek/routes/home.js
+++ pseek/routes/home.js
@@ -5,15 +5,18 @@ var db = require('../lib/db.js');
 // home으로 이동
 router.get('/', (req, res) => {
     // 아티스트 4명만 가져옴
-    var sql = 'SELECT userName, userId FROM `usertbl` WHERE userArtist = 1 LIMIT 4';
-    db().query(sql, (err, results) => {
-        if(!err) {
-            res.render("index.ejs",{
-                artistNames: results
-            });
-            db().end();
-        }
-    });
+    // var sql = 'SELECT userName, userId FROM `usertbl` WHERE userArtist = 1 LIMIT 4';
+    // db().query(sql, (err, results) => {
+    //     if(!err) {
+    //         res.render("index.ejs",{
+    //             artistNames: results
+    //         });
+    //         db().end();
+    //     }
+    // });
+    res.render("./artwork/abstract.ejs",{
+        //             artistNames: results
+                });
 });
 
 module.exports = router;
