32a19704eb7f1904c2537ed5eb1ee9b092677307
hyunjiyoo <solteee23@gmail.com>
2019-02-02 21:35:46 +0900

    artistpick 구현 성공!!

--- pseek/routes/home.js
+++ pseek/routes/home.js
@@ -96,7 +96,7 @@ router.get('/myPage', (req, res) => {
     var sql = 'SELECT art_id, art_imgsrc, art_title FROM `art_tbl` WHERE `art_tbl`.art_id IN (SELECT `pick_tbl`.art_id FROM `pick_tbl` WHERE `pick_tbl`.user_id = ?)';
     db().query(sql, [req.session.userId], (err, results) => {
         var pickArt = results;
-        //
+        // pick테이블에서 현재 로그인된 user_id에 해당하는 pick 아티스트를 artist_id로 조회하여 art테이블에서 아티스트이미지와 이름 조회.
         var sql = 'SELECT user_imgsrc, user_name FROM user_tbl WHERE user_tbl.user_id IN (SELECT pick_tbl.artist_id FROM pick_tbl WHERE pick_tbl.user_id = ?)';
         db().query(sql, [req.session.userId], (err, results) => {
             res.render("myPage.ejs",{
@@ -145,7 +145,7 @@ fs.readdir('./views/artwork', (err, filelist) => {
                 res.redirect('/' + req.params.genre);
             });
         });
-        // DISLIKE 버튼 눌렀을 때 pick_tbl에 데이터 INSERT
+        // DISLIKE 버튼 눌렀을 때 pick_tbl에 데이터 DELETE
         router.post('/:genre/dislike/:id', (req, res) => {
             // req.params객체로 삭제할 art_id를 가져와서 pick테이블에서 DELETE 수행.
             var sql = 'DELETE FROM `pick_tbl` WHERE `pick_tbl`.art_id = (SELECT `art_tbl`.art_id FROM `art_tbl` WHERE `art_tbl`.art_id = ' + "'" + req.params.id + "'" + ')';
@@ -159,7 +159,38 @@ fs.readdir('./views/artwork', (err, filelist) => {
 
 // 작가페이지로 이동
 router.get('/artist', (req, res) => {
-   res.render('artist.ejs', {});
+    var sql ='SELECT * FROM `user_tbl`';
+    db().query(sql, (err, results) => {
+        var artistList = results;
+        console.log(results.length);
+        // 로그인된 user_id를 pick테이블에서 조회하여 해당 user테이블에 있는 값 모두 조회.
+        var sql ='SELECT * FROM `user_tbl` WHERE `user_tbl`.user_id IN (SELECT `pick_tbl`.artist_id FROM `pick_tbl` WHERE `pick_tbl`.user_id = ?)';
+        db().query(sql, [req.session.userId], (err, results) => {
+            console.log(results.length);
+            res.render('artist.ejs', {
+                artistList: artistList,
+                artistpick: results
+            });
+        });
+    });
+});
+// LIKE 버튼 눌렀을 때 pick_tbl에 데이터 INSERT
+router.post('/artist/like/:id', (req,res) => {
+    // req.params객체로 user_id를 가져와서 user테이블에서 해당 값에 대한 데이터를 pick테이블에 INSERT 수행.
+    var sql = 'INSERT INTO pick_tbl VALUES (NULL, ?, NULL, (SELECT user_id FROM user_tbl WHERE user_id = ?))';
+    db().query(sql, [req.session.userId, req.params.id], (err, results) => {
+        if(err) throw err;
+        res.redirect('/artist');
+    });
+});
+// DISLIKE 버튼 눌렀을 때 pick_tbl에 데이터 DELETE
+router.post('/artist/dislike/:id', (req,res) => {
+    // req.params객체로 삭제할 user_id를 가져와서 pick테이블에서 DELETE 수행.
+    var sql = 'DELETE FROM `pick_tbl` WHERE `pick_tbl`.artist_id = (SELECT `user_tbl`.user_id FROM `user_tbl` WHERE `user_tbl`.user_id = ?)';
+    db().query(sql, [req.params.id], (err, results) => {
+        if(err) throw err;
+        res.redirect('/artist');
+    });
 });
 
 
